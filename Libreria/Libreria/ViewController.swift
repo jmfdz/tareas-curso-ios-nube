430a4df8d8029f2b9fa0786d11aa454139762aa7
jmfdz <josema.fernandez@ua.es>
2016-09-24 13:51:24 +0200

    Terminada semana 1

---
+++ Libreria/Libreria/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  libreria
+//
+//  Created by Jose Maria Fernandez on 24/9/16.
+//  Copyright © 2016 Universidad de Alicante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    var resultado : String = ""
+    @IBOutlet weak var isbn: UITextField!
+    @IBOutlet weak var contenido: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        isbn.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func limpiarDatos(_ sender: AnyObject) {
+        isbn.text = ""
+        contenido.text = ""
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        obtenerDatos()
+        return true
+    }
+    
+    func obtenerDatos() -> Void {
+        let direccion = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        let url = URL(string: direccion + self.isbn.text!)
+        
+        let session = URLSession.shared
+        let bloque = { (datos: Data?, respuesta: URLResponse?, error: Error?) -> Void in
+            let valor = String(data:datos!,encoding: .utf8)!
+            DispatchQueue.main.sync() {
+                self.contenido.text = valor
+            }
+        }
+        
+        let dt = session.dataTask(with: url!, completionHandler: bloque)
+        dt.resume()
+    }
+    
+}
+
